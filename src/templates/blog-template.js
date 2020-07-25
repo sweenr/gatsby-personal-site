59f93595b0b95c416c15d7433b83cbe500114e62
Richard Sween <sweenr418@gmail.com>
2020-07-25 15:41:42 -0500

    Add blog metatada to page template

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -5,11 +5,68 @@ import Layout from '../components/layout'
 import Img from 'gatsby-image'
 
 function BlogTemplate({ data }) {
-  const { markdownRemark } = data
+  const { markdownRemark, site } = data
   const { frontmatter, html } = markdownRemark
+  console.log(frontmatter)
+  const { siteMetadata } = site
   return (
     <Layout>
-      <Helmet>
+      <Helmet
+        meta={[
+          {
+            name: `description`,
+            content: frontmatter.description || siteMetadata.description,
+          },
+          {
+            property: `og:title`,
+            content: frontmatter.title || siteMetadata.title,
+          },
+          {
+            property: `og:description`,
+            content: frontmatter.description || siteMetadata.description,
+          },
+          {
+            property: `og:image`,
+            content:
+              siteMetadata.siteUrl +
+              frontmatter.featuredImage.childImageSharp.fluid.src,
+          },
+          {
+            name: `og:image:alt`,
+            content: frontmatter.featuredImageAlt,
+          },
+          {
+            property: `og:type`,
+            content: `article`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: frontmatter.author || siteMetadata.author,
+          },
+          {
+            name: `twitter:title`,
+            content: frontmatter.title || siteMetadata.title,
+          },
+          {
+            name: `twitter:description`,
+            content: frontmatter.description || siteMetadata.description,
+          },
+          {
+            name: `twitter:image`,
+            content:
+              siteMetadata.siteUrl +
+              frontmatter.featuredImage.childImageSharp.fluid.src,
+          },
+          {
+            name: `twitter:image:alt`,
+            content: frontmatter.featuredImageAlt,
+          },
+        ]}
+      >
         <title>{frontmatter.title}</title>
       </Helmet>
       <div id="main" className="alt">
@@ -40,10 +97,20 @@ export default BlogTemplate
 
 export const pageQuery = graphql`
   query($slug: String!) {
+    site {
+      siteMetadata {
+        author
+        description
+        siteUrl
+        title
+      }
+    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
         title
+        description
+        author
         date(formatString: "MMMM DD, YYYY")
         featuredImage {
           childImageSharp {
